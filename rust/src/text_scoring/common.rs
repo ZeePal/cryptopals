fd2f670d6fbc82bb5f038e43913b1e17059f52da
Paul Harrington <pallsjh@gmail.com>
2020-05-14 17:21:08 +1000

    nicer to read!

--- rust/src/text_scoring/common.rs
+++ rust/src/text_scoring/common.rs
@@ -1,7 +1,4 @@
-const COMMON_CHARS: &'static [u8] = &[
-    ' ' as u8, 'A' as u8, 'E' as u8, 'I' as u8, 'O' as u8, 'U' as u8, 'a' as u8, 'e' as u8,
-    'i' as u8, 'o' as u8, 'u' as u8,
-];
+const COMMON_CHARS: &'static [u8] = "AEIOUaeiou ".as_bytes();
 
 pub fn count<T: AsRef<[u8]>>(raw_data: T) -> usize {
     let data = raw_data.as_ref();
