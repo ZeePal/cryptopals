d6cc78ff3f5b480a3314946c067f52555aca5723
Paul Harrington <pallsjh@gmail.com>
2020-05-19 15:06:28 +1000

    cleaner

--- rust/src/text_scoring/common.rs
+++ rust/src/text_scoring/common.rs
@@ -1,4 +1,4 @@
-const COMMON_CHARS: &'static [u8] = b"AEIOUaeiou ";
+const COMMON_CHARS: &[u8] = b"AEIOUaeiou ";
 
 pub fn count<T: AsRef<[u8]>>(raw_data: T) -> usize {
     let data = raw_data.as_ref();
